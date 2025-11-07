9ffb475fb79e1017eed09da3438737c7b4ef7dad
JungHo-K1m <qopqop55@naver.com>
2025-11-07 18:11:11 +0900

    Check Backend

--- backend/src/chat/dto/verify-requirements.dto.ts
+++ backend/src/chat/dto/verify-requirements.dto.ts
@@ -1,5 +1,10 @@
+import { IsOptional, IsNotEmpty } from 'class-validator';
+
 export class VerifyRequirementsDto {
+  @IsNotEmpty()
   requirements: any;
+
+  @IsOptional()
   projectId?: string;
 }
 
