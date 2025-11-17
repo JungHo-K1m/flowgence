e090154b477feaafb6c68c00f38ba379e5e401d2
JungHo-K1m <qopqop55@naver.com>
2025-11-17 15:21:20 +0900

    Check Dto

--- backend/src/notion/dto/notion-oauth-callback.dto.ts
+++ backend/src/notion/dto/notion-oauth-callback.dto.ts
@@ -1,5 +1,11 @@
+import { IsString, IsOptional } from 'class-validator';
+
 export class NotionOAuthCallbackDto {
+  @IsString()
   code: string;
+
+  @IsOptional()
+  @IsString()
   state?: string;
 }
 
